4ba278367b1c1f9b48a842ed023b904c9359b1d4
Jared Sprague <jared@caramelcode.com>
2019-01-15 13:56:26 -0500

    added initial model import for obj and mtl files

--- src/main.js
+++ src/main.js
@@ -4,6 +4,12 @@ import { map } from "../node_modules/lodash-es/lodash.js";
 import app from "./app.js";
 import STATES from "./states.js";
 
+let container;
+let camera, scene, renderer;
+let mouseX = 0, mouseY = 0;
+let windowHalfX = window.innerWidth / 2;
+let windowHalfY = window.innerHeight / 2;
+
 async function start() {
     // fetch the commands database
     const rsp = await fetch("../assets/commands.json");
@@ -15,6 +21,120 @@ async function start() {
     app.commands = commands;
 
     console.log(commandNames);
+
+    init();
+    animate();
 }
+
+
+function init() {
+
+    container = document.createElement('div');
+    document.body.appendChild(container);
+
+    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 2000);
+    camera.position.z = 250;
+
+    // scene
+
+    scene = new THREE.Scene();
+
+    var ambientLight = new THREE.AmbientLight(0xcccccc, 0.4);
+    scene.add(ambientLight);
+
+    var pointLight = new THREE.PointLight(0xffffff, 0.8);
+    camera.add(pointLight);
+    scene.add(camera);
+
+    // model
+
+    var onProgress = function (xhr) {
+
+        if (xhr.lengthComputable) {
+
+            var percentComplete = xhr.loaded / xhr.total * 100;
+            console.log(Math.round(percentComplete, 2) + '% downloaded');
+
+        }
+
+    };
+
+    var onError = function () {
+    };
+
+
+    new THREE.MTLLoader()
+        .setPath('../assets/models/')
+        .load('CLH_Computer.mtl', function (materials) {
+
+            materials.preload();
+
+            new THREE.OBJLoader()
+                .setMaterials(materials)
+                .setPath('../assets/models/')
+                .load('CLH_Computer.obj', function (object) {
+
+                    object.position.y = -250;
+                    object.position.x -= 100;
+                    scene.add(object);
+
+                }, onProgress, onError);
+
+        });
+
+    //
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    container.appendChild(renderer.domElement);
+
+    camera.position.z = 500;
+
+    document.addEventListener('mousemove', onDocumentMouseMove, false);
+
+    //
+
+    window.addEventListener('resize', onWindowResize, false);
+
+}
+
+function onWindowResize() {
+
+    windowHalfX = window.innerWidth / 2;
+    windowHalfY = window.innerHeight / 2;
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+}
+
+function onDocumentMouseMove(event) {
+
+    mouseX = (event.clientX - windowHalfX) / 2;
+    mouseY = (event.clientY - windowHalfY) / 2;
+
+}
+
+function animate() {
+
+    requestAnimationFrame(animate);
+    render();
+
+}
+
+function render() {
+
+    camera.position.x += (mouseX - camera.position.x) * .05;
+    camera.position.y += (-mouseY - camera.position.y) * .05;
+
+    camera.lookAt(scene.position);
+
+    renderer.render(scene, camera);
+
+}
+
 
 start();
