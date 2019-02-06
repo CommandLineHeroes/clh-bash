694d9ae27fce1655726bc0e9c2fa2d797410ecfe
Michael Clayton <mclayton@redhat.com>
2019-02-06 14:47:32 -0500

    four golden cmds per lang

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
     GOLDEN_CMDS_COMMON_PER_LANG: 2, // Number of very common commands to include
-    GOLDEN_CMDS_RANDOM_PER_LANG: 3, // Number of totally random commands to include per language
+    GOLDEN_CMDS_RANDOM_PER_LANG: 2, // Number of totally random commands to include per language
     GOLDEN_CMDS_MAX_LENGTH: 7, // Max string length that a golden command can be
     GOLDEN_CMDS_PREVIEW_TIME: 5000
 };
